785fb42fae2df96ad175639b86ea7dd9db475535
Jan Kopriva <jan.kopriva@gooddata.com>
2012-02-28 14:28:53 +0100

    Changes due to the oh-my-zsh

---
+++ local_settings.zsh
@@ -0,0 +1,16 @@
+export EDITOR=vim
+export PAGER=less
+export HOST=midgard
+export NODE_PATH=/usr/local/lib/node:/usr/local/lib/jsctags
+export JSTESTDRIVER_HOME=~/
+
+alias -r vi="vim"
+alias -r ll="ls -al"
+alias -r rspec="bundle exec rspec"
+alias -r mysql="/usr/local/mysql/bin/mysql"
+alias -r ssh-jklab="ssh root@jklab.getgooddata.com"
+alias -r ssh-jkbugfix="ssh root@jk-bugfix.getgooddata.com"
+alias -r ssh-text="ssh -A textextextcz@server7.railshosting.cz"
+alias -r vi_apache_config="vi /private/etc/apache2/other/gdc.conf"
+alias -r rgs="rake generate_script_tags"
+alias -r sar="sudo apachectl restart"
